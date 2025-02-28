a713c8cd4135d4662e220dc08aac72ce7540b6b2
Sadik Sourov <sadiksourov11@gmail.com>
2025-03-01 03:45:01 +0600

    theme changer done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// document.getElementById("theme-changer").addEventListener("click", function(){
-//     const colors = ["blue", "orange", "tomato", "teal"];
-//     const randomColors = Math.random()*4;
-//     const selectedColor = colors[index];
-//     document.body.style.backgroundColor = selectedColor;
+document.getElementById("theme-changer").addEventListener("click", function(){
+    const colors = ["indigo", "orange", "tomato", "teal", "violet"]
+    const randomNumber = Math.random()*5;
+    const index = Math.round(randomNumber);
+    const color= colors[index];
+    document.body.style.backgroundColor = color;
    
-// })
+})
