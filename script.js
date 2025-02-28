76bbec370adc237435e06a80e5515b7f542c14be
Sadik Sourov <sadiksourov11@gmail.com>
2025-03-01 03:27:30 +0600

    html structure done

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("theme-changer").addEventListener("click", function(){
+//     const colors = ["blue", "orange", "tomato", "teal"];
+//     const randomColors = Math.random()*4;
+//     const selectedColor = colors[index];
+//     document.body.style.backgroundColor = selectedColor;
+   
+// })
