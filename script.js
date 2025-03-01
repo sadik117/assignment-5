1e99156216045985fc6d5ebc3782c4c7958efbbc
Sadik Sourov <sadiksourov11@gmail.com>
2025-03-01 20:15:40 +0600

    70% done

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("theme-changer").addEventListener("click", function(){
-    const colors = ["indigo", "orange", "tomato", "teal", "violet"]
-    const randomNumber = Math.random()*5;
-    const index = Math.round(randomNumber);
-    const color= colors[index];
-    document.body.style.backgroundColor = color;
-   
-})
